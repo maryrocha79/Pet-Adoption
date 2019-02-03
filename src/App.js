fb7c2b98c5437edfdb82d8555c097065731e1465
Mary Rocha <maryrocha79@gmail.com>
2019-02-02 21:41:42 -0800

    added Pet component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Adopt Me!</h1>
+        <Pet name="Lucas" animal="dog" breed="german-pointer" />
+      </div>
+    );
+  }
+}
+
+render(React.createElement(App), document.getElementById("root"));
