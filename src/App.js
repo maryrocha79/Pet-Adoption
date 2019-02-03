889c9db852ef948e69ff03f088b39113b2145882
Mary Rocha <maryrocha79@gmail.com>
2019-02-03 15:04:42 -0800

    added Details component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
 import { Router } from "@reach/router";
+import { Link } from "@reach/router";
 import Results from "./Results";
 import Details from "./Details";
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Adopt me!</h1>
+        <header>
+          <Link to="/">Adopt me!</Link>
+        </header>
+
         <Router>
           <Results path="/" />
           <Details path="/details/:id" />
